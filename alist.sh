4f25e6d480dacafecf19e7ed0dc7222383fc2fbb
Ma2049 <55687675+sztuxp@users.noreply.github.com>
2022-05-08 22:46:49 +0800

    Update alist.sh

--- alist.sh
+++ alist.sh
@@ -1,16 +1,3 @@
-# 初始化rclone配置
-mkdir -p /.config/rclone
-cat >/.config/rclone/rclone.conf <<EOF 
-$rclone
-EOF
-
-mkdir -p /alist/config/temp
-cat >/alist/config/config.json <<EOF
-$alist
-EOF
-
-#复制网盘的备份配置到运行环境
-$(command -v rclone) ${parameter}
 
 cd /alist
 ./alist -conf ./config/config.json
