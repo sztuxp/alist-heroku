000f8445cddf66e81a812f36d19c1d4a5ea0e6a4
Ma2049 <55687675+sztuxp@users.noreply.github.com>
2022-05-08 12:58:14 +0800

    Update alist.sh

--- alist.sh
+++ alist.sh
@@ -3,10 +3,12 @@ mkdir -p /.config/rclone
 cat << EOF > /.config/rclone/rclone.conf
 $rclone
 EOF
-
+mkdir -p /alist/config/temp
 cat >/alist/config/config.json <<EOF
 $alist
 EOF
-
 # 启动rclone挂载
 exec $(command -v rclone) mount ${parameter}
+
+cd /alist
+./alist -conf config/config.json
