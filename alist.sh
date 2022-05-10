39423df65c4c53b45d5ab2df5001bc3b5f02b023
Ma2049 <55687675+sztuxp@users.noreply.github.com>
2022-05-10 20:41:31 +0800

    Update alist.sh

--- alist.sh
+++ alist.sh
@@ -5,8 +5,33 @@ $rclone
 EOF
 
 mkdir -p /alist/config/temp
+
 cat >/alist/config/config.json <<EOF
-$alist
+{
+  "address": "0.0.0.0",
+  "port": $PORT,
+  "assets": "/",
+  "database": {
+    "type": "sqlite3",
+    "user": "",
+    "password": "",
+    "host": "",
+    "port": 0,
+    "name": "",
+    "table_prefix": "x_",
+    "db_file": "/alist/data.db"
+  },
+  "scheme": {
+    "https": false,
+    "cert_file": "",
+    "key_file": ""
+  },
+  "cache": {
+    "expiration": 60,
+    "cleanup_interval": 120
+  },
+  "temp_dir": "/alist/config/temp"
+}
 EOF
 
 #复制网盘的备份配置到运行环境
