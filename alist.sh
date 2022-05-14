a2b360c062da2cb7526444deaae677f4a08d9e45
Ma2049 <55687675+sztuxp@users.noreply.github.com>
2022-05-14 09:27:48 +0800

    Update alist.sh

--- alist.sh
+++ alist.sh
@@ -8,29 +8,8 @@ mkdir -p /alist/config/temp
 
 cat >/alist/config/config.json <<EOF
 {
-  "address": "0.0.0.0",
   "port": $PORT,
-  "assets": "/",
-  "database": {
-    "type": "sqlite3",
-    "user": "",
-    "password": "",
-    "host": "",
-    "port": 0,
-    "name": "",
-    "table_prefix": "x_",
-    "db_file": "/alist/data.db"
-  },
-  "scheme": {
-    "https": false,
-    "cert_file": "",
-    "key_file": ""
-  },
-  "cache": {
-    "expiration": 60,
-    "cleanup_interval": 120
-  },
-  "temp_dir": "/alist/config/temp"
+  $alist
 }
 EOF
 
